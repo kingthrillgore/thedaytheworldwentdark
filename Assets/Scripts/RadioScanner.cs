80946ace45113982a18d4ef4d91789cc4796fe8e
Cameron Kilgore <ghostfreeman@gmail.com>
2021-01-30 00:07:17 -0600

    Breakpoint on sound delegate

--- Assets/Scripts/RadioScanner.cs
+++ Assets/Scripts/RadioScanner.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RadioScanner : MonoBehaviour
 {
+    public float FadeoutCooldown = 5f;
+    public bool FadeoutRunning = false;
+    delegate void StartAudioPlay(int num);
+    delegate void KillAudioPlay(int num);
+    StartAudioPlay startAudio;
+    KillAudioPlay killAudio;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +30,23 @@ public class RadioScanner : MonoBehaviour
 
             if (hit.collider.tag == "NPC") {
                 Debug.Log("We are facing an NPC.");
+                FadeoutRunning = true;
+
+                // TODO Send a delegate to the Sound Engine we are facing an NPC
+                //startAudio(1);
+
+                if (FadeoutRunning) {
+                    if (FadeoutCooldown > 0) {
+                        Debug.Log("Coming in Strong");
+                        FadeoutCooldown -= Time.deltaTime;
+                    } else {
+                        Debug.Log("Fadeout Radio");
+                        //TODO delegate to kill sound
+                        //killAudio(1);
+                        FadeoutCooldown = 0;
+                        FadeoutRunning = false;
+                    }
+                }
             }
         }    
     }
