54a4d1441787959044c07db247b368d348d95f47
jerbear666 <jerryclarkmusic@gmail.com>
2021-01-30 08:04:31 -0600

    Sound stuff. Surface Detect with raycast, and animationEvent set up

---
+++ Assets/Scripts/AnimationEvents.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEvents : MonoBehaviour
+{
+    public FMOD.Studio.EventInstance FootstepEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FootstepEvent = FMODUnity.RuntimeManager.CreateInstance(SoundManager.mainAudio.footsteps);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayFootstepSound()
+    {
+        FootstepEvent.start();
+        FootstepEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    }
+}
