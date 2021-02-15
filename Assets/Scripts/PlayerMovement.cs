428af59559b281086d2e62bca5c52ec7d792e5c5
Gerald Clark <farthestempire@gmail.com>
2021-02-14 23:12:19 -0600

    oops there.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController character_controller;
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+
+    private float gravity = 20f;
+
+    public float jump_force = 10f;
+
+    private float vertical_Velocity;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        character_controller = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovethePlayer();
+    }
+
+    void MovethePlayer()
+    {
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f, Input.GetAxis(Axis.VERTICAL)); //because im a fuckin beast
+        move_Direction = transform.TransformDirection(move_Direction); // WORLD STAR
+        move_Direction *= speed * Time.deltaTime; // because we live n a world that uses regular time unity... figure it out. 
+
+        ApplyGravity();
+
+        character_controller.Move(move_Direction); // because character controllers are crazy
+    } // move player
+
+    void ApplyGravity()
+    {
+        if(character_controller.isGrounded)
+        {
+            vertical_Velocity -= gravity * Time.deltaTime;
+
+            //jump 
+            PlayerJump();
+        }
+        else
+        {
+            vertical_Velocity -= gravity * Time.deltaTime;
+        }
+
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+
+    }//apply gravity 
+
+    void PlayerJump()
+    {
+        if(character_controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_Velocity = jump_force;
+        }
+    }
+
+}//class
