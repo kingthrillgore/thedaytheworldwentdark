5d66b7317b78f82ab25c4175aeaeb918836a0d4c
Gerald Clark <farthestempire@gmail.com>
2021-03-27 22:47:12 -0500

    Made the spline for the ocean. Disabled "OceanAMB" GameObject

---
+++ Assets/Scripts/SplineMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplineMover : MonoBehaviour
+{
+    public SplineAudio spline;
+
+    public Transform followObj;
+
+    private Transform thisTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        thisTransform.position = spline.WhereOnSpline(followObj.position);
+    }
+}
